3243d3a82fe47f0e78b4a33d5e173ed11e7f149f
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-03-17 13:44:44 -0400

    login and logout in sessions

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback' => 'sessions#omniauth' #from this tutorial https://medium.com/swlh/google-authentication-strategy-for-rails-5-application-cd37947d2b1b
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  root 'sessions#login'
+  get '/logout' => 'sessions#logout'
+
+
+  #get '*path' => redirect('/')
 end
