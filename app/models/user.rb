4fb599bef8f404bec1776caf7a916d1de3a357eb
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-03-17 13:24:24 -0400

    omniauth added to user model also routes.rb in config

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ApplicationRecord
   has_many :languages
   has_many :situations, through: :languages
   has_many :emotions, through: :situations
+
+
+  def self.from_omniauth(auth)
+    where(email: auth.info.email).first_or_initialize do |user|
+      user.user_name = auth.info.name
+      user.email = auth.info.email
+      user.password = SecureRandom.hex
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   resources :emotions
   resources :situations
   resources :users
+
+  get '/auth/:provider/callback' => 'sessions#omniauth' #from this tutorial https://medium.com/swlh/google-authentication-strategy-for-rails-5-application-cd37947d2b1b
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
