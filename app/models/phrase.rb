bea18931fbfcc67c5eb628dcf5282f9e5b7cad7f
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-03-17 07:42:50 -0400

    phrase many to many with situations through emotions updated, and explained in IMPORTANT comment

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -1,2 +1,9 @@
 class Phrase < ApplicationRecord
+  has_many :emotions
+  has_many :situations, through: :emotions
+
+  #for this to truly be useful, a user should be able to tag an additional EMOTION from a drop down menu
+  #this will cause FLOATING phrases that are not attached to any given situation, so there should be a built-in situation
+  #the built-in situation should always pre-populate before situations appear alphabetically.
+  #the built-in situation would be "CROSS-EMOTIONAL" phrases that the user cannot directly edit.
 end
