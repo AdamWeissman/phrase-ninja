6fb25c4a5c6b55f4cf59304e60a4def4186b88a2
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-03-17 07:29:46 -0400

    checking text for errors... situation migration had phrase instead of user; changed this

--- db/migrate/20200317103405_create_situations.rb
+++ db/migrate/20200317103405_create_situations.rb
@@ -3,7 +3,7 @@ class CreateSituations < ActiveRecord::Migration[6.0]
     create_table :situations do |t|
       t.string :name
       t.boolean :studying_now, default: false
-      t.belongs_to :phrase, null: false, foreign_key: true
+      t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :language, null: false, foreign_key: true
       t.timestamps
     end
