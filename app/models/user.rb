c849e491024bfbae9bf7a5a1d6f918670cccf587
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-03-18 14:54:56 -0400

    added has_secure_password to user model along with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :situations, through: :languages
   has_many :emotions, through: :situations
 
+  has_secure_password
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
 
   def self.from_omniauth(auth)
     where(email: auth.info.email).first_or_initialize do |user|
@@ -11,5 +14,5 @@ class User < ApplicationRecord
       user.password = SecureRandom.hex
     end
   end
-  
+
 end
