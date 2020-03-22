e286e4b81ce1248537f7dca617f085dc64bb7d8a
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-03-22 15:23:33 -0400

    first pass at phrase parsing works... split method tests WORKING

--- app/models/situation.rb
+++ app/models/situation.rb
@@ -4,12 +4,21 @@ class Situation < ApplicationRecord
   has_many :phrases
   has_many :phrase_scores, through: :phrases
 
+  def testit
+    @phrases_for_situations_test
+  end
+
   def whip_up_some_phrases
     if text_blob_for_phrases.present?
       phrase_parser = PhraseParser.new(text_blob_for_phrases)
       uncategorized_phrases = phrase_parser.separate_the_content
-      uncategorized_phrase.each do |x|
-        Phrase.new(english: x)
+      @phrases_for_situations_test = []
+      uncategorized_phrases.each do |x|
+        the_new_phrase = Phrase.create(english: x)
+        the_new_phrase.save
+        the_new_phrase.translate
+        the_new_phrase.save
+        @phrases_for_situations_test << the_new_phrase
       end
     end
   end
