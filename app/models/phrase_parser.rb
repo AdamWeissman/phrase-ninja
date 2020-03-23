c1e0546d5ec0b0618dc3b121ff7a18801ddd8ad8
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-03-22 20:14:56 -0400

    commented out testing stuff for situation and phraseparser now that I know it works

--- app/models/phrase_parser.rb
+++ app/models/phrase_parser.rb
@@ -14,9 +14,11 @@ class PhraseParser
     line.split(":")
   end
 
+  #commenting out tests
+
   def phrases_for_situations
     uncategorized_phrases = separate_the_content
-    phrases_for_situations_test = []
+    #phrases_for_situations_test = []
     uncategorized_phrases.each do |line|
       if line.count(":") == 1
         the_split = line_splitter(line)
@@ -26,12 +28,12 @@ class PhraseParser
         the_new_phrase.save
         the_new_phrase.translate
         the_new_phrase.save
-        phrases_for_situations_test << the_new_phrase
+        #phrases_for_situations_test << the_new_phrase
       else
         next
       end
     end
-    phrases_for_situations_test
+    #phrases_for_situations_test
   end
 
 #this was a nightmare to setup, hours of testing on repl.it I hope it works here.
--- app/models/situation.rb
+++ app/models/situation.rb
@@ -4,14 +4,16 @@ class Situation < ApplicationRecord
   has_many :phrases
   has_many :phrase_scores, through: :phrases
 
-  def testit
-    @x
-  end
+  #commenting out the tests
+
+  #def testit
+  #  @x
+  #end
 
   def whip_up_some_phrases
     if text_blob_for_phrases.present?
       phrase_parser = PhraseParser.new(text_blob_for_phrases)
-      @x = phrase_parser.phrases_for_situations
+      #@x = phrase_parser.phrases_for_situations
     end
   end
 
