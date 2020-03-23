4eedb5cd6063f255dd4194aa59d0ff38ccdf451a
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-03-23 15:12:55 -0400

    got index populating better

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   #resources :phrase_scores
   #resources :phrases
   resources :situations, only: [:index, :new, :create, :edit, :destroy, :patch]
-  #resources :users
+  resources :users
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
