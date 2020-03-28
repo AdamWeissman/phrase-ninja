e0a3ad57376130e2dd1efb377853ff76c634cfbc
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-03-28 10:18:44 -0400

    reset database for next set of testing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_03_17_105813) do
-
+ 
   create_table "phrase_scores", force: :cascade do |t|
     t.string "familiarity_name"
     t.float "familiarity_name_corresponding_points"
