c37510193e5fdf92434b9417ef8e4f9eb0da6fb6
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-03-29 14:40:47 -0400

    phrases_for_situations method in the phrase_parser now parsing and saving correctly. need to make sure update method is also working.

--- app/models/phrase_parser.rb
+++ app/models/phrase_parser.rb
@@ -17,15 +17,16 @@ class PhraseParser
   end
 
   def phrases_for_situations
+    @this_one_user = User.find(user_id)
     uncategorized_phrases = separate_the_content
-    binding.pry
     uncategorized_phrases.each do |line|
       if line.count(":") == 1
         the_split = line_splitter(line)
         the_category = the_split[0].strip
         the_phrase = the_split[1].strip
-        the_new_phrase = Phrase.new(category: the_category, english: the_phrase, situation_id: the_situation_id, score_id: @user.scores.last) #need to interpolate the users actual score
+        the_new_phrase = Phrase.new(category: the_category, english: the_phrase, situation_id: the_situation_id, score_id: @this_one_user.scores.last.id) #need to interpolate the users actual score
         the_new_phrase.save
+        #binding.pry
         the_new_phrase.translate
         the_new_phrase.save
       else
