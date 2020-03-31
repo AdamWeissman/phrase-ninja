d745dc171c484b3aaa399492ce805f7d3645a717
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-03-31 05:52:09 -0400

    progress, manually sending authentication, but permitted is false. Trying to override permitted.

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,9 +1,10 @@
 class ScoresController < ApplicationController
 #before_action :set_user, only: [:index]
 #this will essentially be the flashcards controller
-skip_before_action :verify_authenticity_token
+#skip_before_action :verify_authenticity_token
 
   def index
+    binding.pry
     if logged_in?
       @user = current_user
       @situations = @user.situations.all
@@ -19,6 +20,7 @@ skip_before_action :verify_authenticity_token
   #nothing below this line is working
 
   def show
+    #this will be populate a phrase flash card without a solution
   end
 
   def new
@@ -27,23 +29,25 @@ skip_before_action :verify_authenticity_token
 
 
   def edit
+    #this will populate a phrase flash card with the solution
   end
 
-  def create
-    @score = Score.new(score_params)
+  #def create
+  #  @score = Score.new(score_params)
 
-    respond_to do |format|
-      if @score.save
-        format.html { redirect_to @score, notice: 'Score was successfully created.' }
-        format.json { render :show, status: :created, location: @score }
-      else
-        format.html { render :new }
-        format.json { render json: @score.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #  respond_to do |format|
+  #    if @score.save
+  #      format.html { redirect_to @score, notice: 'Score was successfully created.' }
+  #      format.json { render :show, status: :created, location: @score }
+  #    else
+  #      format.html { render :new }
+  #      format.json { render json: @score.errors, status: :unprocessable_entity }
+  #    end
+  #  end
+  #end
 
   def update
+    #this is where there user selects the score
     respond_to do |format|
       if @score.update(phrasescore_params)
         format.html { redirect_to @score, notice: 'Score was successfully updated.' }
@@ -56,13 +60,13 @@ skip_before_action :verify_authenticity_token
   end
 
 
-  def destroy
-    @score.destroy
-    respond_to do |format|
-      format.html { redirect_to scores_url, notice: 'score was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  #def destroy
+  #  @score.destroy
+  #  respond_to do |format|
+  #    format.html { redirect_to scores_url, notice: 'score was successfully destroyed.' }
+  #    format.json { head :no_content }
+  #  end
+  #end
 
   private
   #def set_user
